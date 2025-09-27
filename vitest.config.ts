93188d8dd7938073d8a0896c654b9320b5159edd
Ayu <hello@ayuhito.com>
2025-09-27 08:45:26 +0100

    ci(test): add coverage reporter (#31)
    * ci(test): add coverage reporter
    
    * ci(test): remove reporter action
    
    * test(coverage): revert reporter status

--- vitest.config.ts
+++ vitest.config.ts
@@ -11,6 +11,16 @@ export default defineConfig({
 			// Exclude problematic symlink fixtures that cause ELOOP errors
 			"**/tests/fs/fixtures/e/symlink",
 		],
+		coverage: {
+			exclude: [
+				"**/node_modules/**",
+				"**/dist/**",
+				"**/.{idea,git,cache,output,temp}/**",
+				"**/{karma,rollup,webpack,vite,vitest,jest,ava,babel,nyc,cypress,tsup,build}.config.*",
+				"**/tests/**",
+				"tsdown.config.ts",
+			],
+		},
 	},
 	server: {
 		watch: {
